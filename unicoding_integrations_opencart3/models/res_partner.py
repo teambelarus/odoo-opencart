0f644fb7e101d72f3ff2958c1a6726c300b23a41
tlavi555 <139497744+tlavi555@users.noreply.github.com>
2023-07-14 02:30:03 +0600

    Delete unicoding_integrations_opencart3 directory

--- unicoding_integrations_opencart3/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    opencartid = fields.Char('OpenCart ID')
-
-    join_date = fields.Datetime(string='Join Date')
-
